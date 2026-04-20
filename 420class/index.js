ae58a01dd3a6784a9928e8a65f9f28585e8d51b7
smileofuka-creator <smileofuka@gmail.com>
2026-04-20 12:36:23 +0800

    done

---
+++ 420class/index.js
@@ -0,0 +1,16 @@
+let container = document.getElementById("container");
+
+function mybutton() {
+  let inputElement = document.getElementById("input-text");
+  let myText = inputElement.value;
+  container.innerHTML = `  <div class="task-item">
+                <div class="item-style">
+                     <input type="checkbox">
+                     <label>${myText}</label>
+                </div>
+                <button class="delete">Delete</button>
+            </div>`;
+  console.log(myText);
+}
+
+let task = document.getElementsByClassName("task-container")[0];
