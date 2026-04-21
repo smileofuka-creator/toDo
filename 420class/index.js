197fd584ecbc74266f5e211a5f2202e88736e3dc
smileofuka-creator <smileofuka@gmail.com>
2026-04-21 20:24:01 +0800

    21ni oroi

--- 420class/index.js
+++ 420class/index.js
@@ -1,14 +1,120 @@
+let input = document.getElementById("inputText");
 let container = document.getElementById("container");
-
 function mybutton() {
-  let inputElement = document.getElementById("input-text");
-  let myText = inputElement.value;
-  container.innerHTML = `  <div class="task-item">
-                <div class="item-style">
-                     <input type="checkbox">
-                     <label>${myText}</label>
-                </div>
-                <button class="delete">Delete</button>
-            </div>`;
-  console.log(myText);
+  let item = document.createElement("taskItem");
+
+  taskItem.textContent = inputText.value;
+
+  container.appendChild("taskItem");
 }
+
+// let container = document.getElementById("container");
+// function mybutton() {
+//   let inputElement = document.getElementById("input-text");
+//   let myText = inputElement.value;
+//   container.innerHTML = `  <div class="task-item">
+//                 <div class="item-style">
+//                      <input type="checkbox">
+//                      <label>${myText}</label>
+//                 </div>
+//                 <button class="delete">Delete</button>
+//             </div>`;
+//   console.log(myText);
+// }
+
+// const todos = [
+//   { taskName: "geree tseverleh", isCompleted: false },
+//   { taskName: " usand oroh", isCompleted: true },
+//   { taskName: "hicheeldee yvah", isCompleted: true },
+// ];
+
+// for (let i = 0; i < todos.length; i++) {
+//   let todo = todos[i];
+//   let todoHtml = `<div class="task-item">
+//                 <div class="item-style">
+//                      <input type="checkbox"/>
+//                      <label>${todo.taskName}</label>
+//                 </div>
+//                 <button class="delete">Delete</button>
+//             </div>`;
+
+//   container.innerHTML += todoHtml;
+// }
+// let tab1 = document.getElementsByClassName("tab1")[0];
+// let tab2 = document.getElementsByClassName("tab2")[0];
+// let tab3 = document.getElementsByClassName("tab3")[0];
+
+// function showAll() {
+//   tab2.style.backgroundColor = "white";
+//   tab2.style.color = "black";
+//   tab3.style.backgroundColor = "white";
+//   tab3.style.color = "black";
+//   tab1.style.backgroundColor = "#3c82f6";
+//   tab1.style.color = "white";
+// }
+// function showActive() {
+//   tab1.style.backgroundColor = "white";
+//   tab1.style.color = "black";
+//   tab3.style.backgroundColor = "white";
+//   tab3.style.color = "black";
+//   tab2.style.backgroundColor = "#3c82f6";
+//   tab2.style.color = "white";
+// }
+// function showCompleted() {
+//   tab1.style.backgroundColor = "white";
+//   tab1.style.color = "black";
+//   tab2.style.backgroundColor = "white";
+//   tab2.style.color = "black";
+//   tab3.style.backgroundColor = "#3c82f6";
+//   tab3.style.color = "white";
+// }
+
+// let me = {
+//   firstName: "Batbayar",
+//   LastName: "Halo",
+//   age: "50",
+//   hobby: []
+// };
+
+// console.log(me.LastName);
+
+// let persona = [
+//   {
+//     age: 13,
+//     name: "btoa",
+//     phoneNumber: 88839483,
+//     gender: "male",
+//   },
+//   {
+//     age: 42,
+//     name: "Lisa",
+//     phoneNumber: 53635465346,
+//     gender: "female",
+//   },
+//   {
+//     age: 45,
+//     name: "btoa",
+//     phoneNumber: 67365365436,
+//     gender: "male",
+//   },
+//   {
+//     age: 325325,
+//     name: "Tina",
+//     phoneNumber: 768548546,
+//     gender: "female",
+//   },
+//   {
+//     age: 32,
+//     name: "Modun",
+//     phoneNumber: 875648765,
+//     gender: "male",
+//   },
+// ];
+
+// for (let i = 0; i < persona.length; i++) {
+//   if (persona[i].gender === "male") {
+//     console.log(persona[i]);
+//   let y =
+// for(let j = 0; j<)}
+
+// }
